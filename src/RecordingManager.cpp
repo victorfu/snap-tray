f2ff0492806067154b3bbcc2f1bbf126f7fb92cc
Victor Fu <supergothere@gmail.com>
2025-12-25 10:41:00 +0800

    refactor(recording): Simplifies output path

--- src/RecordingManager.cpp
+++ src/RecordingManager.cpp
@@ -676,10 +676,7 @@ void RecordingManager::showSaveDialog(const QString &tempOutputPath)
 
     // Check auto-save setting
     bool autoSave = settings.value("recording/autoSave", false).toBool();
-    QString outputDir = settings.value("recording/outputPath").toString();
-    if (outputDir.isEmpty()) {
-        outputDir = QStandardPaths::writableLocation(QStandardPaths::DownloadLocation);
-    }
+    QString outputDir = QStandardPaths::writableLocation(QStandardPaths::DownloadLocation);
 
     // Get extension from current file
     QFileInfo fileInfo(tempOutputPath);
